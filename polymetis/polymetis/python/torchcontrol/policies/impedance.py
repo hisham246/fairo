5add72fcc53420b287828fc062df89a3b9fe4368
hisham246 <hishamkhalil.eng@gmail.com>
2026-02-05 14:32:02 -0500

    disable gravity comp

--- polymetis/polymetis/python/torchcontrol/policies/impedance.py
+++ polymetis/polymetis/python/torchcontrol/policies/impedance.py
@@ -408,10 +408,10 @@ class HybridJointImpedanceControl(toco.PolicyModule):
         torque_feedforward = self.invdyn(
             joint_pos_current, joint_vel_current, torch.zeros_like(joint_pos_current)
         )  # coriolis
-        # torque_out = torque_feedback + torque_feedforward
+        torque_out = torque_feedback + torque_feedforward
         
         # Gravity compensation only
-        torque_out = torch.zeros_like(torque_feedforward)
+        # torque_out = torch.zeros_like(torque_feedforward)
 
 
         return {"joint_torques": torque_out}
